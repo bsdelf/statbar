d294e54353149a2a2814c6fe7da6b7dfaaafb9d0
Yanhui Shen <shen.elf@gmail.com>
2024-09-21 12:13:34 +0800

    Add unit test for string extension

--- Statbar/Utils/String.swift
+++ Statbar/Utils/String.swift
@@ -3,25 +3,29 @@
 //  statbar
 //
 //  Created by bsdelf on 2020/3/29.
-//  Copyright © 2020 bsdelf. All rights reserved.
+//  Copyright © 2024 bsdelf. All rights reserved.
 //
 
 import Foundation
 
-extension String {
+func getPadding(input: String, targetLength: Int, padString: String) -> String {
+    if (input.count >= targetLength || padString.isEmpty) {
+        return ""
+    }
+    let padCount = (targetLength - input.count) / padString.count
+    let remCount = targetLength - input.count - padString.count * padCount
+    let padding = String(repeating: padString, count: padCount) + padString.prefix(remCount)
+    return padding
+}
+
+public extension String {
     func padStart(targetLength: Int, padString: String = " ") -> String {
-        if targetLength >= self.count {
-            return self
-        }
-        let padding = String(repeating: padString, count: targetLength - self.count)
+        let padding = getPadding(input: self, targetLength: targetLength, padString: padString)
         return padding + self
     }
     
     func padEnd(targetLength: Int, padString: String = " ") -> String {
-        if targetLength >= self.count {
-            return self
-        }
-        let padding = String(repeating: padString, count: targetLength - self.count)
+        let padding = getPadding(input: self, targetLength: targetLength, padString: padString)
         return self + padding
     }
 }
---
+++ StatbarTests/StringExtensionTests.swift
@@ -0,0 +1,42 @@
+//
+//  StatbarTests.swift
+//  StatbarTests
+//
+//  Created by Artoo Detoo on 2024/9/21.
+//  Copyright © 2024 bsdelf. All rights reserved.
+//
+
+import Testing
+@testable import Statbar
+
+struct StringExtensionTests {
+
+    @Test("Pads a string from the start", arguments: [
+        ("", 0, "", ""),
+        ("a", 0, "", "a"),
+        ("a", 1, "", "a"),
+        ("a", 2, "", "a"),
+        ("a", 2, " ", " a"),
+        ("a", 2, ".", ".a"),
+        ("a", 5, "12", "1212a"),
+        ("a", 6, "12", "12121a"),
+    ]) func padStart(str: String, targetLength: Int, padString: String, expected: String) async throws {
+        let actual = str.padStart(targetLength: targetLength, padString: padString)
+        #expect(actual == expected)
+    }
+
+    @Test("Pads a string at the end", arguments: [
+        ("", 0, "", ""),
+        ("a", 0, "", "a"),
+        ("a", 1, "", "a"),
+        ("a", 2, "", "a"),
+        ("a", 2, " ", "a "),
+        ("a", 2, ".", "a."),
+        ("a", 5, "12", "a1212"),
+        ("a", 6, "12", "a12121"),
+    ]) func padEnd(str: String, targetLength: Int, padString: String, expected: String) async throws {
+        let actual = str.padEnd(targetLength: targetLength, padString: padString)
+        #expect(actual == expected)
+    }
+
+}
